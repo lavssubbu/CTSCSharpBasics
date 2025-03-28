79413667f59f1ffcf4687581847a9c3def1621ae
lavssubbu <63305669+lavssubbu@users.noreply.github.com>
2025-03-28 10:24:02 +0530

    ArrayBasics

---
+++ ArrayDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿internal class Program
+{
+    //instance method
+    void FindMax(int[] arr)
+    {
+        int max = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (max < arr[i])
+            {
+                max = arr[i];
+            }
+        }
+        Console.WriteLine("Maximum element" + max);
+    }
+    private static void Main(string[] args)
+    {
+        int[] arr = new int[4];//declaring array
+        arr[1] = 50;
+        arr[3] = 55;
+        //display array
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+
+        int[] ar = { 25, 10, 20, 30, 40, 35 };
+        Program.Show(ar);//static method - using class name,we call the method
+        Program pgm = new Program();//instance creation
+        pgm.FindMax(ar);//Instance method - using instance,we call the method
+
+        Console.WriteLine("Enter the size of an array:");
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        int[] aruser = new int[size];
+        for (int i = 0; i < aruser.Length; i++)
+        {
+            Console.WriteLine($"Enter the value for {i + 1}th/st element:");
+            aruser[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Show(aruser);
+        Array.Sort(aruser);
+        Console.WriteLine();
+        Show(aruser);
+        Array.Reverse(aruser);
+        Console.WriteLine();
+        Show(aruser);
+
+    }
+    public static void Show(int[] ar)
+    {
+        //traverse array using foreach
+        foreach (int val in ar)
+        {
+            Console.WriteLine(val);
+        }
+    }
+}
