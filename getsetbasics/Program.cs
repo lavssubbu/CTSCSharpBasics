1a79ae52744629b03d1d2a317128faa69069ecad
lavssubbu <63305669+lavssubbu@users.noreply.github.com>
2025-03-28 10:25:13 +0530

    getsetbasics

---
+++ getsetbasics/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace getsetsamp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Stid = 11;
+            student.Name = "Anu";
+            student.Age = 28;
+            Console.WriteLine(student.Stid);
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.Age);
+
+        }
+    }
+}
---
+++ getsetbasics/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getsetsamp
+{
+    internal class Student
+    {
+        private int sid;
+
+        public int Stid
+        {
+            get
+            {
+                return sid;
+            }
+            set
+            {
+                sid = value;
+            }
+        }
+
+        private string sname;
+
+        public string Name
+        {
+            get
+            {
+                return sname;
+            }
+            set
+            {
+                if(value.ToLower().Equals("anu"))
+                {
+                    Console.WriteLine("Anu not allowed.Exceptional");
+                }
+                else
+                {
+                    sname = value;
+                }
+            }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if(value>25)
+                {
+                    Console.WriteLine("Not Eligible to attend the exam.Age should be below 25");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+
+    }
+}
